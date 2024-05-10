4f161da4354f0c31cf3baef6f1b579dfd8645842
Rudi Setiyawan <diphons@gmail.com>
2024-05-10 19:46:35 +0700

    oprofile: set profile when screen on-off * profile battery when screen off Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -41,6 +41,9 @@
 
 extern struct frame_stat fm_stat;
 extern void sde_crtc_fod_ui_ready(struct dsi_display *display, int type, int value);
+#ifdef CONFIG_D8G_SERVICE
+static int oprofile_last = -1; // get last profile by oprofile
+#endif
 
 enum dsi_dsc_ratio_type {
 	DSC_8BPC_8BPP,
@@ -4530,6 +4533,8 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_LP1);
 #ifdef CONFIG_D8G_SERVICE
 	oplus_panel_status = 3; // DISPLAY_POWER_DOZE
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
 #endif
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP1 cmd, rc=%d\n",
@@ -4559,6 +4564,8 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 
 #ifdef CONFIG_D8G_SERVICE
 	oplus_panel_status = 4; // DISPLAY_POWER_DOZE_SUSPEND
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
 #endif
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_LP2);
@@ -4636,6 +4643,9 @@ exit_skip:
 
 #ifdef CONFIG_D8G_SERVICE
 	oplus_panel_status = 2; // DISPLAY_POWER_ON
+	if (oprofile_last < 0) 
+		oprofile_last = oprofile;
+	oprofile = oprofile_last; // set oprofile to last profile setted
 #endif
 
 exit:
@@ -5055,6 +5065,9 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 #ifdef CONFIG_D8G_SERVICE
 	oplus_panel_status = 2; // DISPLAY_POWER_ON
+	if (oprofile_last < 0) 
+		oprofile_last = oprofile;
+	oprofile = oprofile_last; // set oprofile to last profile setted
 #endif
 
 	mutex_unlock(&panel->panel_lock);
@@ -5270,6 +5283,8 @@ int dsi_panel_disable(struct dsi_panel *panel)
 
 #ifdef CONFIG_D8G_SERVICE
 	oplus_panel_status = 0; // DISPLAY_POWER_OFF
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
 #endif
 
 	mutex_unlock(&panel->panel_lock);
